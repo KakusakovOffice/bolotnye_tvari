673ec6d3b44dd5d78ea0f7b1229d79a10d2aeadd
KakusakovOffice <99006858+KakusakovOffice@users.noreply.github.com>
2022-04-14 17:19:02 +0300

    I love fucking with DOGS

---
+++ main.py
@@ -0,0 +1,19 @@
+import telebot as tb
+import traceback
+from commands import *
+from time import sleep
+
+from config import bot
+
+def main_polling_loop():
+    while True:
+        try:
+            bot.polling()
+        except BaseException:
+            traceback.format_exc()
+            sleep(3)
+
+
+if __name__ == '__main__':
+    main_polling_loop()
+
